3c8b4d6e9a0ad6e71599d045122867dfecac5654
LislPisl <eroesch@student.unimelb.edu.au>
2020-09-07 15:21:52 +1000

    auto naming of files

--- test/supercritical_pitchfork.jl
+++ test/supercritical_pitchfork.jl
@@ -1,5 +1,6 @@
 include("../src/includes.jl")
 
+bifur_type = "supercritical_pitchfork"
 test_list_alpha, test_list_sigma = Array(range(-15., stop = 15.0, length = 9)), Array(range(-0., stop = 2.0, length = 4))
 tspan = (0.0, 2.0)
 timepoints = 0.2:0.2:2.0
@@ -7,8 +8,8 @@ initpoints = range(-5., stop = 5.0, length = 10)
 npoints = 20 # keeping this low is good for performance
 test_prob_arg_list = [f_supercritical_pitchfork, g_multiplicative_noise, tspan, initpoints, timepoints, npoints]
 @time rep_output_raw, rep_output_potential = rep_solve(test_prob_arg_list, test_list_alpha, test_list_sigma) #first dim is alpha, second dim is sigma
-file_path_raw = "test/data/rep_output_supercritical_raw.jld"
-file_path_potential = "test/data/rep_output_supercritical_potential.jld"
+file_path_raw = string("test/data/rep_output_", bifur_type, "_raw.jld")
+file_path_potential = string("test/data/rep_output_", bifur_type, "_potential.jld")
 save(file_path_raw, "rep_output_raw", rep_output_raw)
 save(file_path_potential, "rep_output_potential", rep_output_potential)
 
@@ -20,29 +21,27 @@ cols = ["#696969", "#920005", "#920005", "#920005"]
 alphas_col = [1.,.4,.6,1.]
 ps_potential = plot_potentials(rep_output_potential, test_list_alpha, test_list_sigma, 1, length(test_list_alpha), 1, 4, cols, alphas_col)
     p_app = plot(ps_potential..., layout = (length(test_list_sigma),length(test_list_alpha)))
-    savefig("test/plots/supercritical_pitchfork_summary_plot_2_potential.pdf")
+    savefig(string("test/plots/", bifur_type, "_summary_plot_2_potential.pdf"))
 
 ps_raw = plot_raw_data(rep_output_raw, test_list_alpha, test_list_sigma, 1, length(test_list_alpha), 1, 4, cols, alphas_col)
     p_app = plot(ps_raw..., layout = (length(test_list_sigma),length(test_list_alpha)))
-    savefig("test/plots/supercritical_pitchfork_summary_plot_2_raw.pdf")
-
-
+    savefig(string("test/plots/", bifur_type, "_summary_plot_2_raw.pdf"))
 
 KS_res = KS_vectors(rep_output_raw)
 p = plot_KS_vectors(KS_res, test_list_sigma, test_list_alpha)
 p_all =plot(p[1], p[2], layout=(1,2))
-savefig("test/plots/KS_raw_data.pdf")
+savefig(string("test/plots/", bifur_type, "_KS_raw_data.pdf"))
 KS_res = KS_vectors(rep_output_potential)
 p = plot_KS_vectors(KS_res, test_list_sigma, test_list_alpha)
 p_all =plot(p[1], p[2], layout=(1,2))
-savefig("test/plots/KS_potential_data.pdf")
+savefig(string("test/plots/", bifur_type, "_KS_potential_data.pdf"))
 
 H_res = H_vectors(rep_output_raw)
 p = plot_H_vectors(H_res, test_list_sigma, test_list_alpha)
 p_all =plot(p[1], p[2], layout=(1,2))
-savefig("test/plots/hell_raw_data.pdf")
+savefig(string("test/plots/", bifur_type, "_hell_raw_data.pdf"))
 
 H_res = H_vectors(rep_output_potential)
 p = plot_H_vectors(H_res, test_list_sigma, test_list_alpha)
 p_all =plot(p[1], p[2], layout=(1,2))
-savefig("test/plots/hell_potential_data.pdf")
+savefig(string("test/plots/", bifur_type, "_hell_potential_data.pdf"))
