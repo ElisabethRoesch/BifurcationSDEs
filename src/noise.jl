b230f327186b0a8901e4b43de06866bca0570d16
EisabethRoesch <eroesch@student.unimelb.edu.au>
2021-02-18 16:46:28 +1100

    add additive noise more d model file

--- src/noise.jl
+++ src/noise.jl
@@ -2,4 +2,8 @@ function g_multiplicative_noise(x, p, t) # noiseeq
     dx = p[2]*x[1]
     return [dx]
 end
-# TO DO -- add other noise functions
+
+function g_additive_noise(x, p, t) # noiseeq
+    dx = p[2]+x[1]
+    return [dx]
+end
