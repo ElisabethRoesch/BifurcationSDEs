1bbc9553f30ebd31f0b9c75110d49a1fc05c0c0a
EisabethRoesch <eroesch@student.unimelb.edu.au>
2021-03-02 13:53:48 +1100

    2d case for both kde packages

--- test/2d_tests/2d_model_vary_sigma_and_beta_new_kde.jl
+++ test/2d_tests/2d_model_vary_sigma_and_beta_new_kde.jl
@@ -24,91 +24,91 @@ sigma_low = 0.2
 prob_arg_list = test_prob_arg_list
 test_p = [0.5, 0.25, -0.4, 0.5, sigma_low] # alpha, lambda, beta, c, sigma
 temp_f, temp_g, temp_tspan, initpoints, timepoints, npoints = prob_arg_list
-a,b = solve_prob_multi_d_new(test_prob_arg_list, test_p)
+a,b = solve_prob_2_d_new(test_prob_arg_list, test_p)
 # p_low_1 = contourf(b, title = "beta = -0.4",xlabel = "State 1", ylabel = "State 2", label = "Quasipotential", clims = (-10,0))
 p_low_1 = contourf(b,  clims = (-10,0), legend = :none, xticks =([0:250:1000;], ["0", "0.25", "0.5","0.75","1.0"]),yticks =([0:250:1000;], ["0", "0.25", "0.5","0.75","1.0"]))
 # Setting 2:
 prob_arg_list = test_prob_arg_list
 test_p = [0.5, 0.25, 0.0, 0.5, sigma_low]
 temp_f, temp_g, temp_tspan, initpoints, timepoints, npoints = prob_arg_list
-a,b = solve_prob_multi_d(test_prob_arg_list, test_p)
+a,b = solve_prob_2_d_new(test_prob_arg_list, test_p)
 # p_low_2 = contourf(b, title = "beta = 0.0", xlabel = "State 1", ylabel = "State 2", label = "Quasipotential", clims = (-10,0))
 p_low_2 = contourf(b,clims = (-10,0), legend = :none, xticks =([0:250:1000;], ["0", "0.25", "0.5","0.75","1.0"]),yticks =([0:250:1000;], ["0", "0.25", "0.5","0.75","1.0"]))
 
-#
-# # Setting 3:
-# prob_arg_list = test_prob_arg_list
-# test_p = [0.5, 0.25, 0.4, 0.5, sigma_low]
-# temp_f, temp_g, temp_tspan, initpoints, timepoints, npoints = prob_arg_list
-# a,b = solve_prob_multi_d(test_prob_arg_list, test_p)
-# # p_low_3 = contourf(b, title = "beta = 0.4", xlabel = "State 1", ylabel = "State 2", label = "Quasipotential", clims = (-10,0))
-# p_low_3 = contourf(b, clims = (-10,0), legend = :none, xticks =([0:250:1000;], ["0", "0.25", "0.5","0.75","1.0"]),yticks =([0:250:1000;], ["0", "0.25", "0.5","0.75","1.0"]))
-#
-# plot(p_low_1, p_low_2, p_low_3)
-#
-#
-#
-# # EXAMPLE 2: Three settings (three different values for beta) for MEDIUM noise (sigma = 0.4)
-# # Setting 1:
-# sigma_medium = 0.4
-# prob_arg_list = test_prob_arg_list
-# test_p = [0.5, 0.25, -0.4, 0.5, sigma_medium] # alpha, lambda, beta, c, sigma
-# temp_f, temp_g, temp_tspan, initpoints, timepoints, npoints = prob_arg_list
-# a,b = solve_prob_multi_d(test_prob_arg_list, test_p)
-# # p_medium_1 = contourf(b, title = "beta = -0.4",xlabel = "State 1", ylabel = "State 2", label = "Quasipotential", clims = (-10,0))
-# p_medium_1 = contourf(b, clims = (-10,0), legend = :none, xticks =([0:250:1000;], ["0", "0.25", "0.5","0.75","1.0"]),yticks =([0:250:1000;], ["0", "0.25", "0.5","0.75","1.0"]))
-#
-# # Setting 2:
-# prob_arg_list = test_prob_arg_list
-# test_p = [0.5, 0.25, 0.0, 0.5, sigma_medium]
-# temp_f, temp_g, temp_tspan, initpoints, timepoints, npoints = prob_arg_list
-# a,b = solve_prob_multi_d(test_prob_arg_list, test_p)
-# # p_medium_2 = contourf(b, title = "beta = 0.0", xlabel = "State 1", ylabel = "State 2", label = "Quasipotential", clims = (-10,0))
-# p_medium_2 = contourf(b, clims = (-10,0), legend = :none, xticks =([0:250:1000;], ["0", "0.25", "0.5","0.75","1.0"]),yticks =([0:250:1000;], ["0", "0.25", "0.5","0.75","1.0"]))
-#
-#
-# # Setting 3:
-# prob_arg_list = test_prob_arg_list
-# test_p = [0.5, 0.25, 0.4, 0.5, sigma_medium]
-# temp_f, temp_g, temp_tspan, initpoints, timepoints, npoints = prob_arg_list
-# a,b = solve_prob_multi_d(test_prob_arg_list, test_p)
-# # p_medium_3 = contourf(b, title = "beta = 0.4", xlabel = "State 1", ylabel = "State 2", label = "Quasipotential", clims = (-10,0))
-# p_medium_3 = contourf(b, clims = (-10,0), legend = :none, xticks =([0:250:1000;], ["0", "0.25", "0.5","0.75","1.0"]),yticks =([0:250:1000;], ["0", "0.25", "0.5","0.75","1.0"]))
-#
-# plot(p_medium_1, p_medium_2, p_medium_3)
-#
-#
-#
-#
-# # EXAMPLE 3: Three settings (three different values for beta) for HIGH noise (sigma = 0.8)
-# # Setting 1:
-# sigma_high = 0.8
-# prob_arg_list = test_prob_arg_list
-# test_p = [0.5, 0.25, -0.4, 0.5, sigma_high] # alpha, lambda, beta, c, sigma
-# temp_f, temp_g, temp_tspan, initpoints, timepoints, npoints = prob_arg_list
-# a,b = solve_prob_multi_d(test_prob_arg_list, test_p)
-# # p_high_1 = contourf(b, title = "beta = -0.4",xlabel = "State 1", ylabel = "State 2", label = "Quasipotential", clims = (-10,0))
-# p_high_1 = contourf(b, clims = (-10,0), legend = :none, xticks =([0:250:1000;], ["0", "0.25", "0.5","0.75","1.0"]),yticks =([0:250:1000;], ["0", "0.25", "0.5","0.75","1.0"]))
-#
-# # Setting 2:
-# prob_arg_list = test_prob_arg_list
-# test_p = [0.5, 0.25, 0.0, 0.5, sigma_high]
-# temp_f, temp_g, temp_tspan, initpoints, timepoints, npoints = prob_arg_list
-# a,b = solve_prob_multi_d(test_prob_arg_list, test_p)
-# # p_high_2 = contourf(b, title = "beta = 0.0", xlabel = "State 1", ylabel = "State 2", label = "Quasipotential", clims = (-10,0))
-# p_high_2 = contourf(b, clims = (-10,0), legend = :none, xticks =([0:250:1000;], ["0", "0.25", "0.5","0.75","1.0"]),yticks =([0:250:1000;], ["0", "0.25", "0.5","0.75","1.0"]))
-#
-#
-# # Setting 3:
-# prob_arg_list = test_prob_arg_list
-# test_p = [0.5, 0.25, 0.4, 0.5, sigma_high]
-# temp_f, temp_g, temp_tspan, initpoints, timepoints, npoints = prob_arg_list
-# a,b = solve_prob_multi_d(test_prob_arg_list, test_p)
-# # p_high_3 = contourf(b, title = "beta = 0.4", xlabel = "State 1", ylabel = "State 2", label = "Quasipotential", clims = (-10,0))
-# p_high_3 = contourf(b, clims = (-10,0), legend = :none, xticks =([0:250:1000;], ["0", "0.25", "0.5","0.75","1.0"]),yticks =([0:250:1000;], ["0", "0.25", "0.5","0.75","1.0"]))
-#
-# plot(p_high_1, p_high_2, p_high_3)
-#
-# plot(p_low_1, p_low_2, p_low_3, p_medium_1, p_medium_2, p_medium_3, p_high_1, p_high_2, p_high_3)
-#
-# savefig("three_noise_levels_three_beta.pdf")
+
+# Setting 3:
+prob_arg_list = test_prob_arg_list
+test_p = [0.5, 0.25, 0.4, 0.5, sigma_low]
+temp_f, temp_g, temp_tspan, initpoints, timepoints, npoints = prob_arg_list
+a,b = solve_prob_2_d_new(test_prob_arg_list, test_p)
+# p_low_3 = contourf(b, title = "beta = 0.4", xlabel = "State 1", ylabel = "State 2", label = "Quasipotential", clims = (-10,0))
+p_low_3 = contourf(b, clims = (-10,0), legend = :none, xticks =([0:250:1000;], ["0", "0.25", "0.5","0.75","1.0"]),yticks =([0:250:1000;], ["0", "0.25", "0.5","0.75","1.0"]))
+
+plot(p_low_1, p_low_2, p_low_3)
+
+
+
+# EXAMPLE 2: Three settings (three different values for beta) for MEDIUM noise (sigma = 0.4)
+# Setting 1:
+sigma_medium = 0.4
+prob_arg_list = test_prob_arg_list
+test_p = [0.5, 0.25, -0.4, 0.5, sigma_medium] # alpha, lambda, beta, c, sigma
+temp_f, temp_g, temp_tspan, initpoints, timepoints, npoints = prob_arg_list
+a,b = solve_prob_2_d_new(test_prob_arg_list, test_p)
+# p_medium_1 = contourf(b, title = "beta = -0.4",xlabel = "State 1", ylabel = "State 2", label = "Quasipotential", clims = (-10,0))
+p_medium_1 = contourf(b, clims = (-10,0), legend = :none, xticks =([0:250:1000;], ["0", "0.25", "0.5","0.75","1.0"]),yticks =([0:250:1000;], ["0", "0.25", "0.5","0.75","1.0"]))
+
+# Setting 2:
+prob_arg_list = test_prob_arg_list
+test_p = [0.5, 0.25, 0.0, 0.5, sigma_medium]
+temp_f, temp_g, temp_tspan, initpoints, timepoints, npoints = prob_arg_list
+a,b = solve_prob_2_d_new(test_prob_arg_list, test_p)
+# p_medium_2 = contourf(b, title = "beta = 0.0", xlabel = "State 1", ylabel = "State 2", label = "Quasipotential", clims = (-10,0))
+p_medium_2 = contourf(b, clims = (-10,0), legend = :none, xticks =([0:250:1000;], ["0", "0.25", "0.5","0.75","1.0"]),yticks =([0:250:1000;], ["0", "0.25", "0.5","0.75","1.0"]))
+
+
+# Setting 3:
+prob_arg_list = test_prob_arg_list
+test_p = [0.5, 0.25, 0.4, 0.5, sigma_medium]
+temp_f, temp_g, temp_tspan, initpoints, timepoints, npoints = prob_arg_list
+a,b = solve_prob_2_d_new(test_prob_arg_list, test_p)
+# p_medium_3 = contourf(b, title = "beta = 0.4", xlabel = "State 1", ylabel = "State 2", label = "Quasipotential", clims = (-10,0))
+p_medium_3 = contourf(b, clims = (-10,0), legend = :none, xticks =([0:250:1000;], ["0", "0.25", "0.5","0.75","1.0"]),yticks =([0:250:1000;], ["0", "0.25", "0.5","0.75","1.0"]))
+
+plot(p_medium_1, p_medium_2, p_medium_3)
+
+
+
+
+# EXAMPLE 3: Three settings (three different values for beta) for HIGH noise (sigma = 0.8)
+# Setting 1:
+sigma_high = 0.8
+prob_arg_list = test_prob_arg_list
+test_p = [0.5, 0.25, -0.4, 0.5, sigma_high] # alpha, lambda, beta, c, sigma
+temp_f, temp_g, temp_tspan, initpoints, timepoints, npoints = prob_arg_list
+a,b = solve_prob_2_d_new(test_prob_arg_list, test_p)
+# p_high_1 = contourf(b, title = "beta = -0.4",xlabel = "State 1", ylabel = "State 2", label = "Quasipotential", clims = (-10,0))
+p_high_1 = contourf(b, clims = (-10,0), legend = :none, xticks =([0:250:1000;], ["0", "0.25", "0.5","0.75","1.0"]),yticks =([0:250:1000;], ["0", "0.25", "0.5","0.75","1.0"]))
+
+# Setting 2:
+prob_arg_list = test_prob_arg_list
+test_p = [0.5, 0.25, 0.0, 0.5, sigma_high]
+temp_f, temp_g, temp_tspan, initpoints, timepoints, npoints = prob_arg_list
+a,b = solve_prob_2_d_new(test_prob_arg_list, test_p)
+# p_high_2 = contourf(b, title = "beta = 0.0", xlabel = "State 1", ylabel = "State 2", label = "Quasipotential", clims = (-10,0))
+p_high_2 = contourf(b, clims = (-10,0), legend = :none, xticks =([0:250:1000;], ["0", "0.25", "0.5","0.75","1.0"]),yticks =([0:250:1000;], ["0", "0.25", "0.5","0.75","1.0"]))
+
+
+# Setting 3:
+prob_arg_list = test_prob_arg_list
+test_p = [0.5, 0.25, 0.4, 0.5, sigma_high]
+temp_f, temp_g, temp_tspan, initpoints, timepoints, npoints = prob_arg_list
+a,b = solve_prob_2_d_new(test_prob_arg_list, test_p)
+# p_high_3 = contourf(b, title = "beta = 0.4", xlabel = "State 1", ylabel = "State 2", label = "Quasipotential", clims = (-10,0))
+p_high_3 = contourf(b, clims = (-10,0), legend = :none, xticks =([0:250:1000;], ["0", "0.25", "0.5","0.75","1.0"]),yticks =([0:250:1000;], ["0", "0.25", "0.5","0.75","1.0"]))
+
+plot(p_high_1, p_high_2, p_high_3)
+
+plot(p_low_1, p_low_2, p_low_3, p_medium_1, p_medium_2, p_medium_3, p_high_1, p_high_2, p_high_3)
+
+savefig("three_noise_levels_three_beta_new.pdf")
