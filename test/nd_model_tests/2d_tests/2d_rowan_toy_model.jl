243e44b891669c09069740ff6be2218ab05367f1
EisabethRoesch <eroesch@student.unimelb.edu.au>
2021-03-12 11:16:48 +1100

    add results

--- test/nd_model_tests/2d_tests/2d_rowan_toy_model.jl
+++ test/nd_model_tests/2d_tests/2d_rowan_toy_model.jl
@@ -9,32 +9,48 @@ timepoints = [1000.0] # poistions of data point selected from SDE solve
 len = 10 # number inits
 # TO DO: how to set inits?
 initpoints = [Array(range(-2, stop = 2, length = len)), Array(range(-2, stop = 2, length = len))]
-npoints = 200 # this is number of SDEs
+npoints = 1000 # this is number of SDEs
 # TO DO: how to set kde_grid?
 kde_grid = [-2:0.1:2, -2:0.1:2]
 test_prob_arg_list = [f_2d_rowan_toy, g_nd_additive, tspan, initpoints, timepoints, npoints, kde_grid]
 
 
 # EXAMPLE 1: Three settings (three different values for beta) for LOW noise (sigma = 0.4)
-sigma_low = 0.4
+sigma_low = 0.2
 # Setting 1:
-test_p = [0.5, 0.25, -0.05, 0.5, sigma_low] # alpha, lambda, beta, c, sigma
+key =  "1_1"
+test_p = [0.5, 0.25, -0.4, 0.5, sigma_low] # alpha, lambda, beta, c, sigma
 a,b = solve_prob_2d_new(test_prob_arg_list, test_p)
+file_path_raw = string("test/nd_model_tests/2d_tests/data/rep_output_",key, "_raw.jld")
+file_path_potential = string("test/nd_model_tests/2d_tests/data/rep_output_", key, "_potential.jld")
+save(file_path_raw, key, a)
+save(file_path_potential, key, b)
+
 # p_low_1 = contourf(b, title = "beta = -0.4",xlabel = "State 1", ylabel = "State 2", label = "Quasipotential", clims = (-10,0))
 p_low_1 = contourf(b, size = (500, 500))
 # Setting 2:
+key =  "1_2"
 prob_arg_list = test_prob_arg_list
 test_p = [0.5, 0.25, 0.0, 0.5, sigma_low]
 temp_f, temp_g, temp_tspan, initpoints, timepoints, npoints = prob_arg_list
 a,b = solve_prob_2d_new(test_prob_arg_list, test_p)
+file_path_raw = string("test/nd_model_tests/2d_tests/data/rep_output_",key, "_raw.jld")
+file_path_potential = string("test/nd_model_tests/2d_tests/data/rep_output_", key, "_potential.jld")
+save(file_path_raw, key, a)
+save(file_path_potential, key, b)
 # p_low_2 = contourf(b, title = "beta = 0.0", xlabel = "State 1", ylabel = "State 2", label = "Quasipotential", clims = (-10,0))
 # p_low_2 = contourf(b,clims = (-10,0), legend = :none, xticks =([0:250:1000;], ["0", "0.25", "0.5","0.75","1.0"]), yticks = ([0:250:1000;], ["0", "0.25", "0.5","0.75","1.0"]))
 p_low_2 = contourf(b, size = (500, 500))
 # Setting 3:
+key =  "1_3"
 prob_arg_list = test_prob_arg_list
 test_p = [0.5, 0.25, 0.4, 0.5, sigma_low]
 temp_f, temp_g, temp_tspan, initpoints, timepoints, npoints = prob_arg_list
 a,b = solve_prob_2d_new(test_prob_arg_list, test_p)
+file_path_raw = string("test/nd_model_tests/2d_tests/data/rep_output_",key, "_raw.jld")
+file_path_potential = string("test/nd_model_tests/2d_tests/data/rep_output_", key, "_potential.jld")
+save(file_path_raw, key, a)
+save(file_path_potential, key, b)
 # p_low_3 = contourf(b, title = "beta = 0.4", xlabel = "State 1", ylabel = "State 2", label = "Quasipotential", clims = (-10,0))
 # p_low_3 = contourf(b, clims = (-10,0), legend = :none, xticks =([0:250:1000;], ["0", "0.25", "0.5","0.75","1.0"]), yticks = ([0:250:1000;], ["0", "0.25", "0.5","0.75","1.0"]))
 p_low_3 = contourf(b, size = (500, 500))
@@ -45,30 +61,45 @@ plot(p_low_1, p_low_2, p_low_3, layout = (1, 3), size = (1500, 500))
 
 # EXAMPLE 2: Three settings (three different values for beta) for MEDIUM noise (sigma = 0.4)
 # Setting 1:
+key =  "2_1"
 sigma_medium = 0.4
 prob_arg_list = test_prob_arg_list
 test_p = [0.5, 0.25, -0.4, 0.5, sigma_medium] # alpha, lambda, beta, c, sigma
 temp_f, temp_g, temp_tspan, initpoints, timepoints, npoints = prob_arg_list
 a,b = solve_prob_2d_new(test_prob_arg_list, test_p)
+file_path_raw = string("test/nd_model_tests/2d_tests/data/rep_output_",key, "_raw.jld")
+file_path_potential = string("test/nd_model_tests/2d_tests/data/rep_output_", key, "_potential.jld")
+save(file_path_raw, key, a)
+save(file_path_potential, key, b)
 # p_medium_1 = contourf(b, title = "beta = -0.4",xlabel = "State 1", ylabel = "State 2", label = "Quasipotential", clims = (-10,0))
 # p_medium_1 = contourf(b, clims = (-10,0), legend = :none, xticks =([0:250:1000;], ["0", "0.25", "0.5","0.75","1.0"]),yticks =([0:250:1000;], ["0", "0.25", "0.5","0.75","1.0"]))
 p_medium_1 = contourf(b, size = (500, 500))
 
 # Setting 2:
+key =  "2_2"
 prob_arg_list = test_prob_arg_list
 test_p = [0.5, 0.25, 0.0, 0.5, sigma_medium]
 temp_f, temp_g, temp_tspan, initpoints, timepoints, npoints = prob_arg_list
 a,b = solve_prob_2d_new(test_prob_arg_list, test_p)
+file_path_raw = string("test/nd_model_tests/2d_tests/data/rep_output_",key, "_raw.jld")
+file_path_potential = string("test/nd_model_tests/2d_tests/data/rep_output_", key, "_potential.jld")
+save(file_path_raw, key, a)
+save(file_path_potential, key, b)
 # p_medium_2 = contourf(b, title = "beta = 0.0", xlabel = "State 1", ylabel = "State 2", label = "Quasipotential", clims = (-10,0))
 # p_medium_2 = contourf(b, clims = (-10,0), legend = :none, xticks =([0:250:1000;], ["0", "0.25", "0.5","0.75","1.0"]),yticks =([0:250:1000;], ["0", "0.25", "0.5","0.75","1.0"]))
 p_medium_2 = contourf(b, size = (500, 500))
 
 
 # Setting 3:
+key =  "2_3"
 prob_arg_list = test_prob_arg_list
 test_p = [0.5, 0.25, 0.4, 0.5, sigma_medium]
 temp_f, temp_g, temp_tspan, initpoints, timepoints, npoints = prob_arg_list
 a,b = solve_prob_2d_new(test_prob_arg_list, test_p)
+file_path_raw = string("test/nd_model_tests/2d_tests/data/rep_output_",key, "_raw.jld")
+file_path_potential = string("test/nd_model_tests/2d_tests/data/rep_output_", key, "_potential.jld")
+save(file_path_raw, key, a)
+save(file_path_potential, key, b)
 # p_medium_3 = contourf(b, title = "beta = 0.4", xlabel = "State 1", ylabel = "State 2", label = "Quasipotential", clims = (-10,0))
 # p_medium_3 = contourf(b, clims = (-10,0), legend = :none, xticks =([0:250:1000;], ["0", "0.25", "0.5","0.75","1.0"]),yticks =([0:250:1000;], ["0", "0.25", "0.5","0.75","1.0"]))
 p_medium_3 = contourf(b, size = (500, 500))
@@ -80,30 +111,45 @@ plot(p_medium_1, p_medium_2, p_medium_3)
 
 # EXAMPLE 3: Three settings (three different values for beta) for HIGH noise (sigma = 0.8)
 # Setting 1:
+key =  "3_1"
 sigma_high = 0.8
 prob_arg_list = test_prob_arg_list
 test_p = [0.5, 0.25, -0.4, 0.5, sigma_high] # alpha, lambda, beta, c, sigma
 temp_f, temp_g, temp_tspan, initpoints, timepoints, npoints = prob_arg_list
 a,b = solve_prob_2d_new(test_prob_arg_list, test_p)
+file_path_raw = string("test/nd_model_tests/2d_tests/data/rep_output_",key, "_raw.jld")
+file_path_potential = string("test/nd_model_tests/2d_tests/data/rep_output_", key, "_potential.jld")
+save(file_path_raw, key, a)
+save(file_path_potential, key, b)
 # p_high_1 = contourf(b, title = "beta = -0.4",xlabel = "State 1", ylabel = "State 2", label = "Quasipotential", clims = (-10,0))
 # p_high_1 = contourf(b, clims = (-10,0), legend = :none, xticks =([0:250:1000;], ["0", "0.25", "0.5","0.75","1.0"]),yticks =([0:250:1000;], ["0", "0.25", "0.5","0.75","1.0"]))
 p_high_1 = contourf(b, size = (500, 500))
 
 # Setting 2:
+key =  "3_2"
 prob_arg_list = test_prob_arg_list
 test_p = [0.5, 0.25, 0.0, 0.5, sigma_high]
 temp_f, temp_g, temp_tspan, initpoints, timepoints, npoints = prob_arg_list
 a,b = solve_prob_2d_new(test_prob_arg_list, test_p)
+file_path_raw = string("test/nd_model_tests/2d_tests/data/rep_output_",key, "_raw.jld")
+file_path_potential = string("test/nd_model_tests/2d_tests/data/rep_output_", key, "_potential.jld")
+save(file_path_raw, key, a)
+save(file_path_potential, key, b)
 # p_high_2 = contourf(b, title = "beta = 0.0", xlabel = "State 1", ylabel = "State 2", label = "Quasipotential", clims = (-10,0))
 # p_high_2 = contourf(b, clims = (-10,0), legend = :none, xticks =([0:250:1000;], ["0", "0.25", "0.5","0.75","1.0"]),yticks =([0:250:1000;], ["0", "0.25", "0.5","0.75","1.0"]))
 p_high_2 = contourf(b, size = (500, 500))
 
 
 # Setting 3:
+key =  "3_3"
 prob_arg_list = test_prob_arg_list
 test_p = [0.5, 0.25, 0.4, 0.5, sigma_high]
 temp_f, temp_g, temp_tspan, initpoints, timepoints, npoints = prob_arg_list
 a,b = solve_prob_2d_new(test_prob_arg_list, test_p)
+file_path_raw = string("test/nd_model_tests/2d_tests/data/rep_output_",key, "_raw.jld")
+file_path_potential = string("test/nd_model_tests/2d_tests/data/rep_output_", key, "_potential.jld")
+save(file_path_raw, key, a)
+save(file_path_potential, key, b)
 # p_high_3 = contourf(b, title = "beta = 0.4", xlabel = "State 1", ylabel = "State 2", label = "Quasipotential", clims = (-10,0))
 # p_high_3 = contourf(b, clims = (-10,0), legend = :none, xticks =([0:250:1000;], ["0", "0.25", "0.5","0.75","1.0"]),yticks =([0:250:1000;], ["0", "0.25", "0.5","0.75","1.0"]))
 p_high_3 = contourf(b, size = (500, 500))
