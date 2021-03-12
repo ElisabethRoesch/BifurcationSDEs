b3ab46faab0a10c186a18a2acdfe83c1bb432169
EisabethRoesch <eroesch@student.unimelb.edu.au>
2021-03-12 16:14:48 +1100

    more ss tests

--- test/nd_model_tests/2d_tests/2d_rowan_toy_model_analysis.jl
+++ test/nd_model_tests/2d_tests/2d_rowan_toy_model_analysis.jl
@@ -1,24 +1,38 @@
-
 include("../../../src/includes.jl")
 
-key = "1_1"
-# load data
-file_path_raw = string("test/nd_model_tests/2d_tests/data/rep_output_", key, "_raw.jld")
-file_path_potential = string("test/nd_model_tests/2d_tests/data/rep_output_", key, "_potential.jld")
-rep_output_raw = load(file_path_raw)[key]
-rep_output_potential = load(file_path_potential)[key]
+
+####
+# Load data
+keys = ["1_1" "1_2" "1_3";
+        "2_1" "2_2" "2_3";
+         "3_1" "3_2" "3_3"]
+set = Matrix(undef, size(keys)[1], size(keys)[2])
+function get_files(key)
+    file_path_raw = string("test/nd_model_tests/2d_tests/data/rep_output_", key, "_raw.jld")
+    file_path_potential = string("test/nd_model_tests/2d_tests/data/rep_output_", key, "_potential.jld")
+    rep_output_raw = load(file_path_raw)[key]
+    rep_output_potential = load(file_path_potential)[key]
+    return rep_output_raw, rep_output_potential
+end
+for i in 1:size(keys)[1]
+    for j in 1:size(keys)[2]
+        set[i,j] = get_files(keys[i,j])
+    end
+end
+#####
+
 
 
 #####
 # Entropy via InformationMeasures.jl
-
-p_1 = [1,2,3]
-p_2 = [1 2 3;
-        4 5 6]
+using InformationMeasures
+p_1 = [1,2,3.3]
+p_2 = [0 0 0;
+        4 5 6.3]
 e_p_1 = InformationMeasures.get_entropy(p_1)
 e_p_2 = InformationMeasures.get_entropy(p_2)
-p2_b = slicematrix(rep_output_potential)
-e_p_2 = InformationMeasures.get_entropy(p_2...)
+p2_b = slicematrix(p_2)
+e_p_2 = InformationMeasures.get_entropy(p2_b...)
 
 
 a = slicematrix(rep_output_potential)
@@ -52,3 +66,30 @@ v_e_p_2 = vonneumann_entropy(p_2) # Error
 s_e = shannon_entropy(a...) # Error
 v_e = vonneumann_entropy(a...) # Error
 ####
+
+using Shannon
+Shannon.entrpopy(p2)
+
+using Statistics, Plots
+q_stds = Matrix(undef, size(keys)[1], size(keys)[2])
+q_vars = Matrix(undef, size(keys)[1], size(keys)[2])
+q_cors = Matrix(undef, size(keys)[1], size(keys)[2])
+q_covs = Matrix(undef, size(keys)[1], size(keys)[2])
+
+for i in 1:size(keys)[1]
+    for j in 1:size(keys)[2]
+        q_stds[i,j] = std(set[i,j][2]) # std of potential
+        q_vars[i,j] = var(set[i,j][2]) # std of potential
+        q_cors[i,j] = cor(set[i,j][2]) # std of potential
+        q_covs[i,j] = cov(set[i,j][2]) # std of potential
+
+    end
+end
+p_stds = contourf(q_stds)
+p_vars = contourf(q_vars)
+p_cors = contourf(q_cors, Nothing)
+p_covs = contourf(q_covs)
+
+
+
+contourf([1 2 3; 1  NaN 3])
---
+++ test/nd_model_tests/summary_stat_ideas.jl
@@ -0,0 +1,26 @@
+using QuantumInformation
+
+shannon_entropy(rep_output_raw)
+vonneumann_entropy(rep_output_raw)
+
+
+using InformationMeasures
+
+get_entropy(rep_output_raw...)
+
+
+
+a = [1,2,3]
+b  = [[1,2,3], [1,2,3]]
+
+rep_output_potential
+s = reshape(rep_output_potential,(1,40))
+
+s = reshape([rep_output_potential...],(1,40))
+v = vec(rep_output_potential)
+reshape(v,(2))
+
+print(b...)
+
+
+aa =  slicematrix(rep_output_potential)
