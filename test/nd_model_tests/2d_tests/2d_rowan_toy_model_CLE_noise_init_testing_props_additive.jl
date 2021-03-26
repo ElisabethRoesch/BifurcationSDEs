ef73b4cdabf3407a59dbc1ca244ca57e8b20c4e6
EisabethRoesch <eroesch@student.unimelb.edu.au>
2021-03-26 14:49:48 +1100

    compare det. part

--- test/nd_model_tests/2d_tests/2d_rowan_toy_model_CLE_noise_init_testing_props_additive.jl
+++ test/nd_model_tests/2d_tests/2d_rowan_toy_model_CLE_noise_init_testing_props_additive.jl
@@ -25,27 +25,10 @@ initpoints = [Array(range(-2, stop = 2, length = len)), Array(range(-2, stop = 2
 npoints = 1000 # this is number of SDEs
 kde_grid = [-2:0.1:2, -2:0.1:2]
 test_prob_arg_list = [f_test, g_nd_additive, tspan, initpoints, timepoints, npoints, kde_grid]
-sigma_low = 0.2
+sigma_low = 0.2 #???
 key =  "1_3"
 test_p = [0.5, 0.25, 0.4, 0.5, sigma_low] # alpha, lambda, beta, c, sigma
-a,b = solve_prob_2d_new(test_prob_arg_list, test_p)
+a_h_f,b_h_f = solve_prob_2d_new(test_prob_arg_list, test_p)
 
 
-scatter(a[1],a[2], label = "")
-
-
-x = [1, 1 ]
-p = test_p
-h1 = 2*p[1]*x[1]
-h2 = 4*p[2]*x[1]^3
-h3 = p[3]
-h4 = 4*p[4]*p[2]*x[2]^3
-h5 = 2*p[4]*p[1]*x[1]
-h6 = 4*p[4]*p[2]*x[1]^3
-h7 = p[4]*p[3]
-h8 = 4*p[2]*x[2]^3
-hs = [h1 h2 h3 h4 h5 h6 h7 h8]'
-S = [1 -1 0 0 1 -1 0 0; 0 0 0 1 0 0 0 -1]
-f = S*hs
-
-f[:,1]
+scatter(a_h_f[1],a_h_f[2], label = "")
